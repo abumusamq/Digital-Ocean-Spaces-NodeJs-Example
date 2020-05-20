5856cd2a01cea5a39049b3ee9bbbe18b3c240598
abumusamq <mkhan3189@gmail.com>
2020-05-20 22:19:28 +1000

    update code

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+router.get('/success', (req, res) => {
+  res.sendFile(__dirname + '/public/success.html');
+});
+
+router.get('/error', (req, res) => {
+  res.sendFile(__dirname + '/public/error.html');
 });
 
 module.exports = router;
--- routes/uploads.js
+++ routes/uploads.js
@@ -1,47 +1,79 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const router = express.Router();
 const multer  = require('multer');
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+const multerS3 = require('multer-s3');
 const AWS = require('aws-sdk');
+
+const SPACES_ENDPOINT = 'nyc3.digitaloceanspaces.com';
+const API_KEY = '';
+const API_SECRET = '';
+const BUCKET_NAME = '';
 
 let space = new AWS.S3({
   //Get the endpoint from the DO website for your space
-  endpoint: "sfo2.digitaloceanspaces.com",
+  endpoint: SPACES_ENDPOINT,
   useAccelerateEndpoint: false,
   //Create a credential using DO Spaces API key (https://cloud.digitalocean.com/account/api/tokens)
-  credentials: new AWS.Credentials("YOUR-KEY-ID-HERE", "YOUR-SECRET-HERE", null)
+  credentials: new AWS.Credentials(API_KEY, API_SECRET, null)
 });
 
-//Name of your bucket here
-const BucketName = "YOUR-BUCKET-NAME-HERE";
+const upload = multer({
+  storage: multerS3({
+    s3: space,
+    bucket: BUCKET_NAME,
+    acl: 'public-read',
+    cacheControl: 'max-age=31536000',
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    key: function (request, file, cb) {
+      console.log(file);
+      cb(null, 'from_node/' + file.originalname);
+    }
+  })
+}).array('upload', 1);
 
-/* Upload file */
-router.post('/upload', upload.single('image'), function(req, res, next) {
-  let uploadParameters = {
-    Bucket: BucketName,
-    ContentType: req.query.content_type,
-    Body: req.file.buffer,
-    ACL: req.query.acl,
-    Key: req.query.file_name
+router.get('/upload-from-local', (req, res, next) => {
+  const filename = path.resolve('./bin/example_image.jpg');
+  console.log(filename);
+  const fileContent = fs.readFileSync(filename);
+  const params = {
+    Bucket: BUCKET_NAME,
+    Key: 'from_node/awesome.jpg',
+    Body: fileContent,
+    CacheControl: 'max-age=31536000',
+    ACL: 'public-read',
+    ContentType: 'image/jpeg',
   };
 
-  space.upload(uploadParameters, function (error, data) {
-    if (error){
-      console.error(error);
-      res.sendStatus(500);
-      return;
+  space.upload(params, (err, data) => {
+      if (err) {
+        console.error(err);
+        return res.send('Failed!!')
+      }
+      console.log(`File uploaded successfully. ${data.Location}`);
+      res.send('Uploaded!')
+  });
+});
+
+router.post('/upload', (req, res, next) => {
+  upload(req, res, function (error) {
+    if (error) {
+      console.log(error);
+      return res.redirect("/error");
     }
-    res.sendStatus(200);
+    console.log('File uploaded successfully.');
+    res.redirect("/success");
   });
 });
 
 
 /* Returns the uploaded file */
-router.get('/:fileName', function (req, res, next) {
+router.get('/:folder/:filename', (req, res, next) => {
+  console.log('querying ' + req.params.folder + '/' + req.params.filename);
   let downloadParameters = {
-    Bucket: BucketName,
-    Key: req.params.fileName
+    Bucket: BUCKET_NAME,
+    Key: req.params.folder + '/' + req.params.filename,
   };
 
   space.getObject(downloadParameters, function(error, data) {
